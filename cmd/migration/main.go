fd14185ab8da0f090eeea1289d8cbe36f1736fa5
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2022-12-28 01:35:12 +0900

    db config from env (migration)

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -4,15 +4,33 @@ import (
 	"agricoladb/ent"
 	"agricoladb/initdb"
 	"context"
+	"fmt"
 	"log"
+	"net"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/caarlos0/env/v6"
+	"github.com/go-sql-driver/mysql"
 )
 
+type config struct {
+	DBHost     string `env:"DB_HOST" envDefault:"localhost"`
+	DBPort     string `env:"DB_PORT" envDefault:"3306"`
+	DBUser     string `env:"DB_USER"`
+	DBPassword string `env:"DB_PASSWORD"`
+	DBName     string `env:"DB_NAME" envDefault:"agricoladb"`
+}
+
 func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("%v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(cfg.getDNS())
 	// ent client
-	client, err := ent.Open("mysql", "user:password@tcp(localhost:3306)/agricoladb")
+	client, err := ent.Open("mysql", cfg.getDNS())
 	if err != nil {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
@@ -31,4 +49,16 @@ func main() {
 	}
 
 	log.Println("success")
+}
+
+func (c *config) getDNS() string {
+	mc := mysql.Config{
+		DBName:               c.DBName,
+		User:                 c.DBUser,
+		Passwd:               c.DBPassword,
+		Addr:                 net.JoinHostPort(c.DBHost, c.DBPort),
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+	}
+	return mc.FormatDSN()
 }
