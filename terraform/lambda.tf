9268bedf40025016d9abc198dd6ffb836866630a
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2023-03-26 14:37:03 +0000

    attach basic execution role for lambda role

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -14,12 +14,22 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+data "aws_iam_policy" "lambda_basic_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = data.aws_iam_policy.lambda_basic_execution.arn
+}
+
 resource "aws_lambda_function" "server" {
   function_name = "agricoladb-server"
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.this.repository_url}:latest"
   role          = aws_iam_role.iam_for_lambda.arn
   architectures = ["arm64"]
+  memory_size   = 512
 
   lifecycle {
     ignore_changes = [
