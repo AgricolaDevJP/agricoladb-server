283ae0ba9fccede788bd132cd105aa3dc71e951e
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2023-03-26 15:30:31 +0000

    src directory

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -56,5 +56,4 @@ resource "aws_api_gateway_deployment" "this" {
 resource "aws_api_gateway_stage" "this" {
   deployment_id = aws_api_gateway_deployment.this.id
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  stage_name    = "production"
 }
