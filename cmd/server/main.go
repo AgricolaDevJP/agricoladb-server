4a6552e8e343929b5bb4fb6b5d0990d33ebd4d03
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2024-01-09 03:31:40 +0900

    refactor: create server package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,20 +7,10 @@ import (
 
 	"entgo.io/ent/dialect"
 	"github.com/AgricolaDevJP/agricoladb-server/ent"
-	"github.com/AgricolaDevJP/agricoladb-server/graph"
-	"github.com/AgricolaDevJP/agricoladb-server/graph/generated"
+	"github.com/AgricolaDevJP/agricoladb-server/internal/server"
 	"github.com/AgricolaDevJP/agricoladb-server/sqlite"
 	"github.com/caarlos0/env/v10"
 	"github.com/go-chi/chi"
-	"github.com/rs/cors"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/playground"
-)
-
-const (
-	maxComplexity = 100
 )
 
 func init() {
@@ -31,6 +21,7 @@ type config struct {
 	Port           string   `env:"PORT" envDefault:"8000"`
 	DBPath         string   `env:"DB_PATH" envDefault:"agricoladb.sqlite"`
 	AllowedOrigins []string `env:"ALLOWED_ORIGINS" envSeparator:","`
+	MaxComplexity  int      `env:"MAX_COMPLEXITY" envDefault:"100"`
 }
 
 func main() {
@@ -40,26 +31,19 @@ func main() {
 	}
 
 	dsn := fmt.Sprintf("file:%s?cache=shared&mode=ro", cfg.DBPath)
-
 	client, err := ent.Open(dialect.SQLite, dsn)
 	if err != nil {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
 	defer client.Close()
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
-		Client: client,
-	}}))
-	srv.Use(extension.FixedComplexityLimit(maxComplexity))
-
 	router := chi.NewRouter()
-	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   cfg.AllowedOrigins,
-		AllowCredentials: true,
-	}).Handler)
-
-	router.Handle("/", playground.Handler("AgricolaDB playground", "/graphql"))
-	router.Handle("/graphql", srv)
+	s := &server.Server{
+		AllowedOrigins: cfg.AllowedOrigins,
+		Client:         client,
+		MaxComplexity:  cfg.MaxComplexity,
+	}
+	s.Install(router)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", cfg.Port)
 	log.Fatal(http.ListenAndServe(":"+cfg.Port, router))
